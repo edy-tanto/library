f5f9f7bdb0baba5d00e9f1e960224a4242cf2939
Edy Tanto <edytanto.chai@gmail.com>
2020-06-26 23:58:59 +0700

    create author test

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorManagementTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('authors', [
+            'name' => 'Author Name',
+            'dob' => '05/14/1988', // date of birth
+        ]);
+
+        $authors = Author::all();
+
+        $this->assertCount(1, $authors);
+        $this->assertInstanceOf(Carbon::class, $authors->first()->dob);
+        $this->assertEquals('1988-05-14', $authors->first()->dob->format('Y-m-d'));
+    }
+
+}
